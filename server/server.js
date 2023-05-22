1b6de5e659a896f22ac1e697aba7d7d63ddd4a35
Tyler Lowe <tlowe3420@gmail.com>
2023-05-21 21:58:50 -0500

    Working on calculation logic on the server

--- server/server.js
+++ server/server.js
@@ -17,10 +17,79 @@ let equationArr = [];
 app.post('/answer', (req, res) => {
     console.log('Adding New Problem To Solve', req.body);
     equationArr.push(req.body);
-    perfromCalculation(equationArr)
+    parseCalculation(equationArr)
     res.sendStatus(201);
 })
 
-function perfromCalculation(){
+let inputArr = [];
 
+function parseCalculation(arr){
+    let string = arr[0].calculation;
+    console.log(string);
+    let value = '';
+    for (const item of string) {
+        if (item === '+') {
+            inputArr.push(value)
+            inputArr.push(item);
+            value = '';
+        }  else if (item === '-') {
+            inputArr.push(value)
+            inputArr.push(item);
+            value = '';
+        } else if (item === '/') {
+            inputArr.push(value)
+            inputArr.push(item);
+            value = '';
+        } else if (item === 'x') {
+            inputArr.push(value)
+            inputArr.push(item);
+            value = '';
+        } else if (item === '=') {
+            inputArr.push(value)
+            inputArr.push(item);
+        } else {
+            value += item;
+        }
+    }
+   
+    perfromCalculation(inputArr);
 }
+
+['7', '+', '7']
+
+function perfromCalculation(arr) {
+    let total = 0;
+    let value = 0;
+    console.log(arr);
+    let counter = 0;
+    for (const item of arr) {
+        if(counter === 0){
+            total = Number(item);
+            } else if(item === '+') {
+                total += Number(value);
+                value = 0;
+            } else if(item === '-') {
+                total -= Number(value);
+                value = 0;
+            } else if(item === '*') {
+                total -= Number(value);
+                value = 0;
+            } else if(item === '/') {
+                total /= valNumber(value);
+                value = 0;
+            } else if(item === '%') {
+                total -= Number(value);
+                value = 0;
+            } else if (item === '=') {
+                break;
+            } else {
+                total += Number(item);
+                console.log(value);
+            }
+            counter++;
+    }
+
+    console.log(total);
+}
+
+
